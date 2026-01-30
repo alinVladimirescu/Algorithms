172af7e12ca4c6013c8f7aebc242719f9b399a6d
alinVladimirescu <alin.vladimirescu@gmail.com>
2026-01-30 09:03:28 +0000

    Exercise 1

--- Practice/Week3/sorting.py
+++ Practice/Week3/sorting.py
@@ -7,6 +7,36 @@ def insertion_sort(arr):
                 arr[i] = arr[j]
                 arr[j] = tmp
     return arr
+def partition(arr, l, h):
+    pivot = arr[h]
+    i = l - 1
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1
+
+def quick_sort(arr, l = 0, h = None):
+    if h is None:
+        h = len(arr) - 1
+    if l < h:
+        pivot_i = partition(arr, l, h)
+        quick_sort(arr, l, pivot_i - 1)
+        quick_sort(arr, pivot_i + 1, h)
+
+def quick_sort_stack(arr):
+    stack = [(0, len(arr) - 1)]
+    while stack:
+        l, h = stack.pop()
+        if l < h:
+            pivot_i = partition(arr, l, h)
+            if pivot_i < h:
+                stack.append((pivot_i + 1, h))
+            else:
+                stack.append((l, pivot_i - 1))
+        
+        
 def mergeSort(arr):
     if len(arr) <= 1:
         return arr
@@ -34,3 +64,6 @@ def merge(left, right):
     return result
 
 print(mergeSort([1, 2, 3, 4, 7, 10, 5]))
+sorted_arr = [8,2,4,7,1,3,9,6,5]
+quick_sort(sorted_arr)
+print(sorted_arr)
